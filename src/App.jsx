7f333924c28b294ae1f8d860cb65195a1c6b918c
Esteban <estebanmauricio.vargas1@alumnos.ulagos.cl>
2024-10-08 15:44:55 -0300

    Proyecto react

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Nav from "./Components/Header-Nav/Nav"
+import Footer from "./Components/Footer/Footer" 
+import Parrafos from "./Components/Parrafos/Parrafos"
+
+
+function App() {
+
+  return (
+    <body>
+      <div className="bg-slate-900">
+        <Nav />
+        <Parrafos />
+        <Footer />
+      </div>
+
+    </body>
+  )
+}
+
+export default App
